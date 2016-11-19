7bea6a37f506c50110ba15dd135ec05e022fe667
Michael Pope <mpope.cr@gmail.com>
2016-11-19 11:32:53 -0800

    Nuke files

--- app/views/counters/_counter.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! counter, :id, :count, :created_at, :updated_at
-json.url counter_url(counter, format: :json)
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :counters, only: :index do
-    collection do
-      post "count"
-    end
-  end
-
-  root to: "counters#index"
-end
